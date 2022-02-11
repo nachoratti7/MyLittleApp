6da40b7347ba18488b6ed58937a374794e846a61
nachoratti09 <nachoratti09@gmail.com>
2022-02-11 16:17:45 -0300

    Initial commit

---
+++ app/src/main/java/primer/practico/mylittleapp/MainActivity.java
@@ -0,0 +1,13 @@
+package primer.practico.mylittleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
